b5b64386c7a480491c0c4b3c77b0405cb7ec3e48
rob <robert.nicholas@brightpearl.com>
2018-01-27 16:22:30 -0500

    Added Deborah talking signaling

--- Deborah/Deborah.gd
+++ Deborah/Deborah.gd
@@ -1,7 +1,15 @@
 extends AnimatedSprite
 
 func _ready():
-	pass
+	var teleprompter = get_tree().get_nodes_in_group("Teleprompter")[0]
+	if teleprompter != null:
+		teleprompter.connect("deborah_talking", self, "_start_talking")
+		teleprompter.connect("deborah_stop_talking", self, "_stop_talking")
 
-func _process(delta):
-	pass
+func _start_talking():
+	print("talking")
+	#animaion.play("deborah_lips")
+	
+func _stop_talking():
+	print("not talking")
+	#animation.stop("deborah_lips")
--- Teleprompter/Teleprompter.gd
+++ Teleprompter/Teleprompter.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
 signal new_chunk(id)
+signal deborah_talking
+signal deborah_stop_talking
 	
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		emit_signal("new_chunk", "test")
+	if Input.is_action_just_pressed("ui_right"):
+		emit_signal("deborah_stop_talking")
+	if Input.is_action_just_pressed("ui_left"):
+		emit_signal("deborah_talking")
