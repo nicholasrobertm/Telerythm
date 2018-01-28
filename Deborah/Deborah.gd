05ace5d56d0dfe4257313e10dda5136cbb5aecc8
rob <robert.nicholas@brightpearl.com>
2018-01-28 14:15:05 -0500

    file cleanup

--- Deborah/Deborah.gd
+++ Deborah/Deborah.gd
@@ -7,11 +7,26 @@ func _ready():
 		teleprompter[0].connect("deborah_stop_talking", self, "_stop_talking")
 	
 
+func _eye_updated(mood,state):
+	if state:
+		if mood:
+			$Eyes.animation.play("calm_blink")
+		else:
+			$Eyes.animation.play("panic_blink")
+	else:
+		if mood:
+			$Eyes.animation.play("calm")
+		else:
+			$Eyes.animation.play("panic")
+
+func _mouth_updated():	
+	
+
 func _calm_to_panic():
-	animation.play("calm_to_panic")
+	$AnimationPlayer.play("calm_to_panic")
 
 func _panic_to_calm():
-	animation.play("panic_to_calm")
+	$AnimationPlayer.play("panic_to_calm")
 
 func _start_talking():
 	print("talking")
