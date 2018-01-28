24d56e9e4124bbef3e63d1eb7f2ee20a7d2f9afe
rob <robert.nicholas@brightpearl.com>
2018-01-28 17:25:00 -0500

    deborah changes

--- Deborah/Deborah.gd
+++ Deborah/Deborah.gd
@@ -18,37 +18,29 @@ func _transition(id, status):
 func _eye_updated(mood,state):
 	if state:
 		if mood:
-			$Eyes.animation.play("calm_blink")
+			$Eyes.play("calm_blink")
 		else:
-			$Eyes.animation.play("panic_blink")
+			$Eyes.play("panic_blink")
 	else:
 		if mood:
-			$Eyes.animation.play("calm")
+			$Eyes.play("calm")
 		else:
-			$Eyes.animation.play("panic")
+			$Eyes.play("panic")
 
 func _mouth_updated(mood, state):	
 	if state:
 		if mood:
-			$Mouth.animation.play("calm_blink")
+			$Mouth.play("calm_blink")
 		else:
-			$Mouth.animation.play("panic_blink")
+			$Mouth.play("panic_blink")
 	else:
 		if mood:
-			$Mouth.animation.play("calm")
+			$Mouth.play("calm")
 		else:
-			$Mouth.animation.play("panic")
+			$Mouth.play("panic")
 
 func _calm_to_panic():
 	$AnimationPlayer.play("calm_to_panic")
 
 func _panic_to_calm():
 	$AnimationPlayer.play("panic_to_calm")
-
-func _start_talking():
-	print("talking")
-	#animaion.play("deborah_lips")
-	
-func _stop_talking():
-	print("not talking")
-	#animation.stop("deborah_lips")
