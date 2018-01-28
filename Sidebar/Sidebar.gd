b63a8168b4690f3747a554f74f030c6ab8ca598f
rob <robert.nicholas@brightpearl.com>
2018-01-28 18:53:28 -0500

    sidebar changes

--- Sidebar/Sidebar.gd
+++ Sidebar/Sidebar.gd
@@ -1,10 +1,20 @@
 extends Node2D	
 
-#func _ready():
-#	var teleprompter = get_tree().get_nodes_in_group("Teleprompter")
-#	if teleprompter.size() != 0:
-#		teleprompter[0].connect("new_chunk", self, "_image_change")
+func _ready():
+	var teleprompter = get_tree().get_nodes_in_group("Teleprompter")
+	if teleprompter.size() != 0:
+		teleprompter[0].connect("new_chunk", self, "_image_change")
 
-#func _image_change(id, time):
-#	self.animation = id
-#	self.play(id)
+func _image_change(id, status):
+	#if id == 0:
+	#	self.animation = "accumulation"
+#		return
+	#else:
+		var message = get_tree().get_nodes_in_group("Teleprompter")[0].messages[id]
+		if 'neutral' in message:
+			self.animation = message['neutral_sidebar']
+			
+		elif 'good' in message:
+			self.animation = message['good_sidebar']
+		elif 'bad' in message:
+			self.animation = message['bad_sidebar']
