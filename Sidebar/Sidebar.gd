63c53c10725a2e2f31f7b407ffdca8f2ae71d161
rob <robert.nicholas@brightpearl.com>
2018-01-28 19:10:15 -0500

    Sidebar finished

--- Sidebar/Sidebar.gd
+++ Sidebar/Sidebar.gd
@@ -6,15 +6,10 @@ func _ready():
 		teleprompter[0].connect("new_chunk", self, "_image_change")
 
 func _image_change(id, status):
-	#if id == 0:
-	#	self.animation = "accumulation"
-#		return
-	#else:
-		var message = get_tree().get_nodes_in_group("Teleprompter")[0].messages[id]
-		if 'neutral' in message:
-			self.animation = message['neutral_sidebar']
-			
-		elif 'good' in message:
-			self.animation = message['good_sidebar']
-		elif 'bad' in message:
-			self.animation = message['bad_sidebar']
+	var message = get_tree().get_nodes_in_group("Teleprompter")[0].messages[id]
+	if 'neutral' in message:
+		self.animation = message['neutral_sidebar']
+	elif 'good' in message:
+		self.animation = message['good_sidebar']
+	elif 'bad' in message:
+		self.animation = message['bad_sidebar']
