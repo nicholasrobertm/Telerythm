bdcce2424352d5683e987d6b26ca65a8c7f8d89e
GalanCM <galan.c.m@gmail.com>
2018-02-01 19:51:25 -0500

    Title tweaks
    · Delay on starting music (fixes glitchiness)
    · Use title screen as splash.

--- Title.gd
+++ Title.gd
@@ -1,5 +1,10 @@
 extends Sprite
 
+func _ready():
+	
+	yield( get_tree().create_timer(1.0), "timeout" )
+	$AudioStreamPlayer.play()
+
 func _input(event):
 	if event.is_action_type():
 		get_tree().change_scene_to( preload("res://Newsroom.tscn") )
